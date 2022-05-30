1ceb99765faa06cc9212e3c820bec9ed995817c1
lubek <borys.kostka09@gmail.com>
2022-05-30 19:12:12 +0200

    Saving

--- app.py
+++ app.py
@@ -6,9 +6,21 @@ import requests
 import json
 from HackChat import app as hcAPI
 import threading
+import configparser
 #sg.Window(title="Hello World", layout=[[]], margins=(100, 50)).read()
 
+configparser_ = configparser.ConfigParser()
+
 version = '0.0.4'
+# checks if config.ini exists
+if not configparser_.read('config.ini'):
+    # if not, create one
+    configparser_.add_section('server')
+    configparser_.set('server', 'url', '')
+    configparser_.add_section('user')
+    configparser_.set('user', 'username', '')
+    configparser_.set('user', 'password', '')
+    configparser_.write(open('config.ini', 'w'))
 #get version from github 
 
 def log_to_file():
@@ -65,10 +77,12 @@ def login_window():
         layout = [[sg.Text('Username', size=(15, 1)) , sg.InputText()],
                     [sg.Text('Password', size=(15, 1)), sg.InputText()],
                     [sg.Button('Login'), sg.Button('Cancel')],
-                    [sg.Text('', size=(15, 1), key='-OUTPUT-')]]
+                    [sg.Checkbox('Remember me')]]
                     
         window = sg.Window('LubChat - Login', layout)
         event, values = window.read()
+        
+
         if event == 'Login':
             #check if all are filled
             if values[0] == '' or values[1] == '':
@@ -83,6 +97,10 @@ def login_window():
                     window.close()
                     login_window()
                 else:
+                    if values[2] == True:
+                        configparser_.set('user', 'username', values[0])
+                        configparser_.set('user', 'password', values[1])
+                        configparser_.write(open('config.ini', 'w'))
                     window.close()
                     return token
 
@@ -97,28 +115,42 @@ if __name__ == '__main__':
         pass
     else:
         sg.popup('A new version is available, please update')
-    layout = [[sg.Text('Enter the url of the server')],
+    if configparser_.get('server', 'url') == '':
+        layout = [[sg.Text('Enter the url of the server')],
                 [sg.InputText(key='url')],
-                [sg.Button('Register'), sg.Button('Login'), sg.Button('Cancel')]]
-    window = sg.Window('Server Settings', layout)
-    event, values = window.read()
-    url = values['url']
-    hcAPI.set_url(url)
-    window.close()
-    if event == 'Register':
-        #register window
+                [sg.Button('Register'), sg.Button('Login'), sg.Button('Cancel')],
+                [sg.Checkbox('Remember me', key='remember')]]
+        window = sg.Window('Server Settings', layout)
+        event, values = window.read()
+        url = values['url']
         hcAPI.set_url(url)
-        response = register_window()
-        print(response)
-        token = None
-    elif event == 'Login':
-        #login window
-        hcAPI.set_url(url)
-        response = login_window()
-        token = response['token']
-    if event == 'Cancel':
-        #quit the program
-        exit()
+        window.close()
+        if event == 'Register':
+            #register window
+            if values['remember'] == True:
+                configparser_.set('server', 'url', url)
+                configparser_.write(open('config.ini', 'w'))
+            
+            hcAPI.set_url(url)
+            response = register_window()
+            print(response)
+            token = None
+        elif event == 'Login':
+            #login window
+            hcAPI.set_url(url)
+            response = login_window()
+            token = response['token']
+        if event == 'Cancel':
+            #quit the program
+            exit()
+    else:
+        hcAPI.set_url(configparser_.get('server', 'url'))
+    if configparser_.get('user', 'username') == '' or configparser_.get('user', 'password') == '':
+        token = login_window()['token']
+
+
+    
+    
     #make a window for chat
     if token == None:
         exit()
