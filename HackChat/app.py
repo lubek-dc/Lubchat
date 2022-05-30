6b6e1526af10ecfa1ebec611f4f2d15fa5b5f052
lubek <borys.kostka09@gmail.com>
2022-05-30 13:52:57 +0200

    Ranks!

--- HackChat/app.py
+++ HackChat/app.py
@@ -2,84 +2,110 @@ import requests as rq
 import json
 # make a variable that will be accessable from functions
 
-class Settings:
-    def set_url(url_):
-        global url
-        url = url_
 
-    def get_url():
-        return url
+def set_url(url_):
+    global url
+    url = url_
 
-    def set_token(token_):
-        global token
-        token = token_
-    def get_token():
-        return token
+
+#get url function
+def get_url():
+    return url
+
+def set_token(token_):
+    global token
+    token = token_
+def get_token():
+    return token
 class User:
     def register(name, password):
-        address = Settings.get_url() + '/register'
+        address = get_url() + '/register'
         payload = json.dumps({'name': name, 'password': password})
         headers = {'content-type': 'application/json'}
         r = rq.post(address, data=payload, headers=headers)
-        return r.json()
+        return r
     def login(name, password):
-        address = Settings.get_url() + '/login'
+        address = get_url() + '/login'
         payload = json.dumps({'name': name, 'password': password})
         headers = {'content-type': 'application/json'}
         r = rq.post(address, data=payload, headers=headers)
+        set_token(r.json()['token'])
         return r.json()
     def change_password(old,new):
-        address = Settings.get_url() + '/change_password'
+        address = get_url() + '/change_password'
         payload = json.dumps({'oldpassword': old, 'newpassword': new})
-        headers = {'content-type': 'application/json'}
+        headers = {'content-type': 'application/json', 'x-access-tokens': get_token()}
         r = rq.post(address, data=payload, headers=headers)
         return r.json()
     def change_name(name):
-        address = Settings.get_url() + '/change_name'
+        address = get_url() + '/change_name'
         payload = json.dumps({'name': name})
-        headers = {'content-type': 'application/json'}
+        headers = {'content-type': 'application/json','x-access-tokens': get_token()}
         r = rq.post(address, data=payload, headers=headers)
         return r.json()
     def get_user_info():
-        address = Settings.get_url() + '/user_info'
-        headers = {'content-type': 'application/json'}
+        address = get_url() + '/user'
+        headers = {'content-type': 'application/json', 'x-access-tokens': get_token()}
         r = rq.get(address, headers=headers)
+        return r.json()
+    def grant_rank(userid,rankid):
+        address = get_url() + '/grant_rank'
+        payload = json.dumps({'userid': userid, 'rankid': rankid})
+        headers = {'content-type': 'application/json', 'x-access-tokens': get_token()}
+        r = rq.post(address, data=payload, headers=headers)
+        return r.json()
+    def revoke_rank(userid):
+        address = get_url() + '/revoke_rank'
+        payload = json.dumps({'userid': userid})
+        headers = {'content-type': 'application/json', 'x-access-tokens': get_token()}
+        r = rq.post(address, data=payload, headers=headers)
+        return r.json()
+    def get_user_by_id(userid):
+        address = get_url() + '/user/' + str(userid)
+        headers = {'content-type': 'application/json', 'x-access-tokens': get_token()}
+        r = rq.get(address, headers=headers)
+        return r.json()
+    def get_user_by_name(name):
+        address = get_url() + '/user/name/' + name
+        headers = {'content-type': 'application/json', 'x-access-tokens': get_token()}
+        r = rq.get(address, headers=headers)
+        return r.json()
 
 class Message:
     def get_last_messages():
-        address = Settings.get_url() + '/messages'
+        address = get_url() + '/messages'
         headers = {'x-access-tokens': token}
         r = rq.get(address, headers=headers)
         return r.json()
     def get_message(id):
-        address = Settings.get_url() + '/message/' + id
+        address = get_url() + '/message/' + id
         headers = {'x-access-tokens': token}
         r = rq.get(address, headers=headers)
         return r.json()
     def send_message(message):
-        address = Settings.get_url() + '/add_message'
+        address = get_url() + '/add_message'
         payload = json.dumps({'message': message})
         headers = {'x-access-tokens': token, 'content-type': 'application/json'}
         r = rq.post(address, data=payload, headers=headers)
         return r.json()
     def delete_message(id):
-        address = Settings.get_url() + '/delete_message/' + id
+        address = get_url() + '/delete_message/' + id
         headers = {'x-access-tokens': token}
         r = rq.delete(address, headers=headers)
         return r.json()
 class Admin:
     def delete_user(id):
-        address = Settings.get_url() + '/delete_user/' + id
+        address = get_url() + '/delete_user/' + id
         headers = {'x-access-tokens': token}
         r = rq.delete(address, headers=headers)
         return r.json()
     def delete_specific_message(id):
-        address = Settings.get_url() + '/delete_specific_message/' + id
+        address = get_url() + '/delete_specific_message/' + id
         headers = {'x-access-tokens': token}
         r = rq.delete(address, headers=headers)
         return r.json()
 
 if __name__ == '__main__':
-    Settings.set_url('http://localhost:5000')
-    Settings.set_token(User.login('Lubekd','LUbeK')['token'])
+    set_url('http://localhost:5000')
+    set_token(User.login('Lubekd','LUbeK')['token'])
     print(User.get_last_messages())
--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 # hello_world.py
 
+from urllib import response
 import PySimpleGUI as sg
 import requests
 import json
 from HackChat import app as hcAPI
 
-global url
 #sg.Window(title="Hello World", layout=[[]], margins=(100, 50)).read()
 
 def register_window():
@@ -15,7 +15,7 @@ def register_window():
                     [sg.Text('Confirm Password', size=(15, 1)), sg.InputText()],
                     [sg.Button('Register'), sg.Button('Cancel')]]
                     
-        window = sg.Window('HackChat - Register', layout)
+        window = sg.Window('LubChat - Register', layout)
         event, values = window.read()
         if event == 'Register':
             #check if all are filled
@@ -31,10 +31,11 @@ def register_window():
                 window.close()
                 register_window()
             else:
-                hcAPI.User.register(values[0], values[1])
+                data = hcAPI.User.register(values[0], values[1])
+                print(data)
                 window.close()
                 sg.popup('You have been registered')
-                login_window()
+                exit()
 
 def login_window():
     #layout: username, password
@@ -43,7 +44,7 @@ def login_window():
                     [sg.Button('Login'), sg.Button('Cancel')],
                     [sg.Text('', size=(15, 1), key='-OUTPUT-')]]
                     
-        window = sg.Window('HackChat - Login', layout)
+        window = sg.Window('LubChat - Login', layout)
         event, values = window.read()
         if event == 'Login':
             #check if all are filled
@@ -74,46 +75,128 @@ if __name__ == '__main__':
     window = sg.Window('Server Settings', layout)
     event, values = window.read()
     url = values['url']
+    hcAPI.set_url(url)
     window.close()
     if event == 'Register':
         #register window
-        hcAPI.Settings.set_url(url)
-        
-        token = register_window()['token']
+        hcAPI.set_url(url)
+        response = register_window()
+        print(response)
+        token = None
     elif event == 'Login':
         #login window
-        hcAPI.Settings.set_url(url)
-        token = login_window()['token']
+        hcAPI.set_url(url)
+        response = login_window()
+        token = response['token']
     if event == 'Cancel':
         #quit the program
         exit()
     #make a window for chat
-    hcAPI.Settings.set_token(token)
-    layout = [[sg.Text('Chat')],
-                [sg.Button('Refresh'), sg.Button('Send'), sg.Button('Logout')],
-                [sg.Multiline(size=(30, 10), key='-OUTPUT-')],
-                [sg.InputText(key='-INPUT-')]]
-    window = sg.Window('Chat', layout)
-    event, values = window.read()
-    while True:
-        #refresh the chat
-        if event == 'Refresh': # example response: [[{'author': 'Lubekd', 'id': 1, 'message': 'Hello'}], 200]
-            messages = hcAPI.Message.get_last_messages()
-            output = ''
-            for message in messages[0]:
-                output += message['author'] + ': ' + message['message'] + '\n'
-            window['-OUTPUT-'].update(output)
-            event, values = window.read()
+    if token == None:
+        exit()
+    #if user is admin 
+#    #Example response of get_user_info
+#    {
+#    "id": 1,
+#    "name": "test",
+#    "password": null,
+#    "public_id": null,
+#    "rankid": 3
+#}
+    userss = hcAPI.User.get_user_info()
+    if userss['rankid'] == 1:
+        layout = [[sg.Text('Admin Chat')],
+                    [sg.Multiline(size=(30, 10), key='-OUTPUT-')],
+                    [sg.InputText(key='-INPUT-')],
+                    [sg.Button('Send'),sg.Button('Refresh') , sg.Button('Logout'), sg.Button('Remove Message'), sg.Button('Grant Permission'), sg.Button('Revoke Permission')]]
+        
+        window = sg.Window('LubChat - Admin Chat', layout)
+        
 
-        #send a message
-        elif event == 'Send':
-            hcAPI.Message.send_message(values['-INPUT-'])
-        #logout
-        elif event == 'Logout':
-            hcAPI.User.logout()
-            exit()
-        #read the message
+        while True:
+            #refresh the chat
+            event, values = window.read()
+            if event == 'Refresh': # example response: [[{'author': 'Lubekd', 'id': 1, 'message': 'Hello'}], 200]
+                messages = hcAPI.Message.get_last_messages()
+                messages = messages['messages']
+                output = ''
+                for message in messages:
+                    output +=  "("+str(message['id'])+") "+ message['author'] + ': ' + message['message'] + '\n'
+                window['-OUTPUT-'].update(output)
+
+            #send a message
+            elif event == 'Send':
+                hcAPI.Message.send_message(values['-INPUT-'])
+            #logout
+            elif event == 'Logout':
+                hcAPI.User.logout()
+                exit()
+            #remove a message
+            elif event == 'Remove Message':
+                hcAPI.Message.delete_message(values['-INPUT-'])
+            #read the message
+            elif event == 'Grant Permission':
+                #make a popup for the user to choose the user
+                layout = [[sg.Text('Choose the user')],
+                            [sg.InputText(key='user')],
+                            [sg.DropDown(['Admin','Moderator','User'], key='type')],
+                            [sg.Button('Grant'), sg.Button('Cancel')]]
+                popup = sg.Window('Grant Permission', layout)
+                event, values = window.read()
+                popup.close()
+                if event == 'Grant':
+                    #Rank id's
+                    #1 - Admin
+                    #2 - Moderator
+                    #3 - User
+                    rankid = 0
+                    if values['type'] == 'Admin':
+                        rankid = 1
+                    elif values['type'] == 'Moderator':
+                        rankid = 2
+                    elif values['type'] == 'User':
+                        rankid = 3
+                    hcAPI.User.grant_permission(values['user'], rankid)
+            elif event == 'Revoke Permission':
+                #make a popup for the user to choose the user
+                layout = [[sg.Text('Choose the user')],
+                            [sg.InputText(key='user')],
+                            [sg.Button('Revoke'), sg.Button('Cancel')]]
+                popup = sg.Window('Revoke Permission', layout)
+                event, values = window.read()
+                popup.close()
+                if event == 'Revoke':
+                    user = hcAPI.User.get_user_by_name(values['user'])
+                    print(user)
+                    hcAPI.User.revoke_permission(user['id'])
+            
+    else:
+        layout = [[sg.Text('Chat')],
+                    [sg.Button('Refresh'), sg.Button('Send'), sg.Button('Logout')],
+                    [sg.Multiline(size=(30, 10), key='-OUTPUT-')],
+                    [sg.InputText(key='-INPUT-')]]
+        window = sg.Window('Chat', layout)
         event, values = window.read()
+        while True:
+            #refresh the chat
+            if event == 'Refresh': # example response: [[{'author': 'Lubekd', 'id': 1, 'message': 'Hello'}], 200]
+                messages = hcAPI.Message.get_last_messages()
+                output = ''
+                for message in messages[0]:
+                    output += message['author'] + ': ' + message['message'] + '\n'
+                window['-OUTPUT-'].update(output)
+                event, values = window.read()
+
+            #send a message
+            elif event == 'Send':
+                hcAPI.Message.send_message(values['-INPUT-'])
+            #logout
+            elif event == 'Logout':
+                hcAPI.User.logout()
+                exit()
+            #read the message
+            event, values = window.read()
+    
         
         
 
